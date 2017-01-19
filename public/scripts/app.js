716dbd18f8bc6698f5054fcd7e79dd8333c3c836
Charlie Garnaas <cgarnaas555@gmail.com>
2017-01-19 17:03:08 -0600

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,58 @@
+var nolan = 0;
+var rukia = 0;
+var charlie =0;
+var person = 0;
+$(function(){
+  console.log('document loaded up');
+
+  $('button').on('click', function(){
+    votes();
+    person = $(this).data('name');
+  });
+
+  getBio();
+});
+
+
+
+function getBio() {
+  $.ajax({
+    url: '/bio',
+    type: 'GET',
+    success: displayBio
+  });
+}
+
+function displayBio(bios) {
+  console.log(bios);
+
+  bios.forEach(function(bio) {
+    $('#person').append('<li>Name: ' + bio.name +  ' Bio: '+ bio.bio  + '</li>');
+    $('#person').append('<button type="button" data-name="' + bio.name + '"><img src="'+ bio.photo +'" alt="image of person"/>LIKE ME!</button>');
+  });
+}
+
+function votes()  {
+  $.ajax({
+    url: '/bio',
+    type: 'GET',
+    success: displayVotes
+  });
+
+}
+
+function displayVotes() {
+
+}
+
+  // function likeCounter() {
+  //   if (person == 'nolan'){
+  //     nolan++;
+  //   }else if (person == 'rukia') {
+  //     rukia++;
+  //   }else if (person == 'charlie') {
+  //     charlie++;
+  //   }
+  //
+  //   }
+  // }
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require ('express');
+var path = require ('path');
+var bodyParser = require ('body-parser');
+var bio = require('./bios.json');
+var app = express();
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function (req, res){
+  res.sendFile(path.join (__dirname, 'public/views/index.html'));
+});
+
+app.get('/bio', function (req, res) {
+  res.send(bio);
+});
+
+app.listen(3000);
+
+
+// function likeCounter() {
+//   if (person == 'nolan'){
+//     nolan++;
+//   }else if (person == 'rukia') {
+//     rukia++;
+//   }else if (person == 'charlie') {
+//     charlie++;
+//   }
+//
+//   }
+// }
